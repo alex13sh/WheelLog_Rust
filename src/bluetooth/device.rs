228a2fc33e824b21c42e08ec1a4c53a830f8ee2d
Алексей (Работа) <alex76sh1@gmail.com>
2022-10-13 22:40:27 +0300

    Вынес Device в отдельный модуль

---
+++ src/bluetooth/device.rs
@@ -0,0 +1,43 @@
+use super::{Peripheral, PeripheralProperties};
+
+#[derive(Debug, Clone)]
+pub struct BlueToothInfo {
+    pub name: String,
+    pub is_connected: bool,
+}
+
+#[derive(Debug, Clone)]
+pub struct Device {
+    p: Peripheral,
+    props: PeripheralProperties,
+    pub info: BlueToothInfo,
+}
+impl Device {
+    pub async fn new(p: Peripheral) -> Self {
+        use btleplug::api::Peripheral;
+
+        let props = p.properties().await.unwrap().unwrap();
+        let is_connected = p.is_connected().await.unwrap();
+        let info = BlueToothInfo {
+            name: props.local_name.clone().unwrap(),
+            is_connected,
+        };
+        Self {
+            p, props, info
+        }
+    }
+    pub async fn update_info(&mut self) {
+        use btleplug::api::Peripheral;
+
+        self.props = self.p.properties().await.unwrap().unwrap();
+        let is_connected = self.p.is_connected().await.unwrap();
+        let info = BlueToothInfo {
+            name: self.props.local_name.clone().unwrap(),
+            is_connected,
+        };
+        self.info = info;
+    }
+    pub fn is_connected(&self) -> bool {
+        self.info.is_connected
+    }
+}
